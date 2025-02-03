902a0a53f39b865725cd07922ddfa1abacf3a795
LucasCTN <lucascampostn@gmail.com>
2025-02-03 00:55:52 -0300

    Faz o endpoint usar os dados do controller através da factory

--- infra/routes/router.ts
+++ infra/routes/router.ts
@@ -1,13 +1,16 @@
 import express, { Express, Request, Response, Router } from "express";
+import { factoryConsultaSalaController } from "../factories/factory-controle-consulta-sala";
 
 const router = Router();
+
+const objeto = factoryConsultaSalaController();
 
 router.get('/', (req: Request, res: Response) => {
     res.send('Hello world');
 });
 
 router.get('/professor/salas', (req: Request, res: Response) => {
-    res.send({ salas: ['sala1', 'sala2', 'sala3'] });
+    res.send(objeto.handle(req, res));
 });
 
 export { router };
