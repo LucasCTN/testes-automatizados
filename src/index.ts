f4709c58f6cc84fafbacf4e535a167c099fcfeee
LucasCTN <lucascampostn@gmail.com>
2025-02-02 22:11:57 -0300

    Inicia projeto com typescript+expressjs

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// src/index.js
+import express, { Express, Request, Response } from "express";
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello world');
+});
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
