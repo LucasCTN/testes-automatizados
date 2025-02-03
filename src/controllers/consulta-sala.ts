061822b275668ec2f35ece41e2aae07ecc3fb82b
LucasCTN <lucascampostn@gmail.com>
2025-02-03 00:42:29 -0300

    Implementa o controller de consulta de salas

--- src/controllers/consulta-sala.ts
+++ src/controllers/consulta-sala.ts
@@ -7,6 +7,24 @@ export interface IController {
 export class ConsultaSalaController implements IController {
 
     public async handle(req: Request, resp: Response): Promise<void> {
-        throw new Error('Method not implemented.');
+        const { valor } = req.params;
+        const minha_resposta = {
+            salas_disponiveis: [{
+                bloco: 'A',
+                numero: 100,
+                lotacao: 10
+            },
+            {
+                bloco: 'A',
+                numero: 101,
+                lotacao: 10
+            },
+            {
+                bloco: 'B',
+                numero: 209,
+                lotacao: 15
+            }],
+        };
+        resp.status(200).json(minha_resposta).end();
     }
 }
