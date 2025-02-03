8357407a6c589613fbef4e5aa59ab35387182a80
LucasCTN <lucascampostn@gmail.com>
2025-02-03 00:38:53 -0300

    Cria teste para o controller

--- tests/controllers/consulta-sala.test.ts
+++ tests/controllers/consulta-sala.test.ts
@@ -1,8 +1,36 @@
 import { ConsultaSalaController } from "../../src/controllers/consulta-sala";
+import { Request, Response } from 'express';
+
+class ResponseFake {
+    statusCodeInformado: number = 0;
+    jsonInformado: any = null;
+    endChamado: boolean = false;
+
+    status(code: number): ResponseFake {
+        this.statusCodeInformado = code;
+        return this;
+    }
+
+    json(data: any): ResponseFake {
+        this.jsonInformado = data;
+        return this;
+    }
+
+    end(): ResponseFake {
+        this.endChamado = true;
+        return this;
+    }
+}
 
 function makeSUT() {
+    const requestStub = {
+        params: {
+        },
+    } as any as Request;
+
+    const responseFake = new ResponseFake();    
     const controller = new ConsultaSalaController();
-    return { controller };
+    return { requestStub, responseFake, controller };
 }
 
 describe('ConsultaSalaController', () => {
@@ -10,4 +38,29 @@ describe('ConsultaSalaController', () => {
         let { controller } = makeSUT();
         expect(controller).toBeDefined();
     });
+});
+
+it('deve chamar handle', async () => {
+    let { requestStub, responseFake, controller } = makeSUT();
+    await controller.handle(requestStub, responseFake as any as Response);
+    
+    expect(responseFake.statusCodeInformado).toBe(200);
+    expect(responseFake.jsonInformado.salas_disponiveis).toBe([
+        {
+            bloco: 'A',
+            numero: 100,
+            lotacao: 10
+        },
+        {
+            bloco: 'A',
+            numero: 101,
+            lotacao: 10
+        },
+        {
+            bloco: 'B',
+            numero: 209,
+            lotacao: 15
+        }
+    ]);
+    
 });
